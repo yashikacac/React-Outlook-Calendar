dd3465a354a7f5d313db01808c958d80ac783442
yashikacac <yashika.cac@gmail.com>
2021-06-21 15:17:01 +0530

    React single-page app that uses the Microsoft Graph API to retrieve calendar information for a user.

---
+++ src/Config.ts
@@ -0,0 +1,9 @@
+export const config = {
+    appId: 'Your Token Key',
+    redirectUri: 'http://localhost:3000',
+    scopes: [
+      'user.read',
+      'mailboxsettings.read',
+      'calendars.readwrite'
+    ]
+  };
